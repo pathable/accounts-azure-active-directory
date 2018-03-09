ae156d9d165b0df124787d2ace6b4eed93122541
Vivero <devivero.c@gmail.com>
2018-03-08 17:49:22 -0800

    update repo information, and dependency versions

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
     summary: "Login service for Azure Active Directory",
-    version: "1.0.0",
-    name: "wiseguyeh:accounts-azure-active-directory",
-    git: "https://github.com/djluck/accounts-azure-active-directory"
+    version: "1.0.1",
+    name: "qipo:accounts-azure-active-directory",
+    git: "https://github.com/qipoco/accounts-azure-active-directory"
 });
 
 Package.onUse(function(api) {
-    api.use(['underscore@1.0.1', 'random@1.0.1']);
-    api.use('accounts-base@1.1.3', ['client', 'server']);
+    api.use(['underscore@1.0.10', 'random@1.0.1']);
+    api.use('accounts-base@1.4.2', ['client', 'server']);
     // Export Accounts (etc) to packages using this one.
     api.imply('accounts-base', ['client', 'server']);
-    api.use('accounts-oauth@1.1.3', ['client', 'server']);
-    api.use('wiseguyeh:azure-active-directory@1.0.0', ['client', 'server']);
+    api.use('accounts-oauth@1.1.15', ['client', 'server']);
+    api.use('qipo:azure-active-directory@1.0.5', ['client', 'server']);
 
     api.addFiles('azure_ad_login_buttons.css', 'client');
 
