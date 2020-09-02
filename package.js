73cb49ad221cc41448fda56248820bcf1a821a2f
filipenevola <filipenevola@gmail.com>
2020-09-02 15:58:12 -0400

    removes pathable namespace

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Login service for Azure Active Directory',
-  version: '1.3.3',
+  version: '1.3.4',
   name: 'pathable:accounts-azure-active-directory',
   git: 'https://github.com/pathable/accounts-azure-active-directory',
 });
@@ -11,8 +11,8 @@ Package.onUse(api => {
   api.use('accounts-base', ['client', 'server']);
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
-  api.use('pathable:accounts-oauth@1.3.3', ['client', 'server']);
-  api.use('pathable:azure-active-directory@1.3.3', ['client', 'server']);
+  api.use('accounts-oauth@1.3.3', ['client', 'server']);
+  api.use('pathable:azure-active-directory@1.3.4', ['client', 'server']);
 
   api.addFiles('azure_ad_login_buttons.css', 'client');
 
