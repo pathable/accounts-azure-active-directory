ba0b931da3b9ee38e5c48d7b0bcc65de1e7dbc8d
Igor Fontana <igor193@gmail.com>
2020-09-02 15:46:58 -0300

    bumping versions

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Login service for Azure Active Directory',
-  version: '1.3.1',
+  version: '1.3.3',
   name: 'pathable:accounts-azure-active-directory',
   git: 'https://github.com/pathable/accounts-azure-active-directory',
 });
@@ -11,8 +11,8 @@ Package.onUse(api => {
   api.use('accounts-base', ['client', 'server']);
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
-  api.use('accounts-oauth', ['client', 'server']);
-  api.use('pathable:azure-active-directory@1.3.1', ['client', 'server']);
+  api.use('pathable:accounts-oauth@1.3.3', ['client', 'server']);
+  api.use('pathable:azure-active-directory@1.3.3', ['client', 'server']);
 
   api.addFiles('azure_ad_login_buttons.css', 'client');
 
