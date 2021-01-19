2e6f13eaa4bf042de7f78668864404f27c7584c3
Igor Fontana <igor193@gmail.com>
2021-01-19 14:09:18 -0300

    Using root url from request

--- package.js
+++ package.js
@@ -1,16 +1,18 @@
 Package.describe({
   summary: 'Login service for Azure Active Directory',
-  version: '1.3.4',
+  version: '1.3.5',
   name: 'pathable:accounts-azure-active-directory',
   git: 'https://github.com/pathable/accounts-azure-active-directory',
 });
 
 Package.onUse(api => {
+  api.versionsFrom('1.11');
+
   api.use('accounts-base', ['client', 'server']);
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('pathable:azure-active-directory@1.3.4', ['client', 'server']);
+  api.use('pathable:azure-active-directory@1.3.5', ['client', 'server']);
 
   api.addFiles('azure_ad_login_buttons.css', 'client');
 
