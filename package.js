899d945f5e37658eff4ac6da483bccd29eb4644f
Igor Fontana <igor193@gmail.com>
2021-08-05 08:03:23 -0300

    TP-15511 Upgrade to Meteor 2.3 - upgrade oauth

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
   summary: 'Login service for Azure Active Directory',
-  version: '1.3.7',
+  version: '1.4.0',
   name: 'pathable:accounts-azure-active-directory',
   git: 'https://github.com/pathable/accounts-azure-active-directory',
 });
 
 Package.onUse(api => {
-  api.versionsFrom('1.12.1');
+  api.versionsFrom('2.3.4');
 
   api.use('accounts-base', ['client', 'server']);
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('pathable:azure-active-directory@1.3.7', ['client', 'server']);
+  api.use('pathable:azure-active-directory@1.4.0', ['client', 'server']);
 
   api.addFiles('azure_ad_login_buttons.css', 'client');
 
